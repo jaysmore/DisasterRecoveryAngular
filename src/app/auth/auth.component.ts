8a16d8e0644aeaa6d7f54fe1546cfb1a15f6fec3
Stefanie Adrianzen <stefanie.adrianzen@gmail.com>
2020-02-27 10:54:35 -0500

    Edited the AuthService. JWT tokens should now be verified with each Http request.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -12,7 +12,7 @@ export class AuthComponent implements OnInit {
   registerMode = false;
   values: any;
   model: any = {};
-  constructor( private authService: AuthService, private alertify: AlertifyService) {}
+  constructor( public authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit(): void {
   }
@@ -28,8 +28,7 @@ login() {
 }
 
 loggedIn() {
-  const token = localStorage.getItem('token');
-  return !!token;
+  return this.authService.loggedIn();
 }
 
 logout(){
